73f4b3062b47a53ebb0c6fbe1253975972ceaa7f
Robert Hardin <robertjameshardin@aol.com>
2022-08-24 16:43:31 -0500

    Changed to simple format for current version

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import PageContent from './components/PageContent';
 import Home from "./pages/Home";
-import Green from "./pages/Green";
-import Red from "./pages/Red";
-import Blue from "./pages/Blue";
-import Yellow from "./pages/Yellow";
+// import Green from "./pages/Green";
+// import Red from "./pages/Red";
+// import Blue from "./pages/Blue";
+// import Yellow from "./pages/Yellow";
 
 function App() {
   const [pages] = useState([
     {
       name: "home"
     },
-    { 
-      name: "green" 
-    },
-    { 
-      name: "red" 
-    },
-    {
-      name: "blue" 
-    },
-    {
-      name: "yellow"
-    }
+    // { 
+    //   name: "green" 
+    // },
+    // { 
+    //   name: "red" 
+    // },
+    // {
+    //   name: "blue" 
+    // },
+    // {
+    //   name: "yellow"
+    // }
   ]);
 
 
@@ -33,14 +33,14 @@ function App() {
     switch (currentPage.name) {
       case 'home':
         return <Home />;
-      case 'red':
-        return <Red />;
-      case 'blue':
-        return <Blue />;
-      case 'yellow':
-        return <Yellow />;
-      case "green":
-        return <Green />;
+      // case 'red':
+      //   return <Red />;
+      // case 'blue':
+      //   return <Blue />;
+      // case 'yellow':
+      //   return <Yellow />;
+      // case "green":
+      //   return <Green />;
       default:
         return <Home />;
     }
@@ -51,11 +51,11 @@ function App() {
   return (
     <div className="container">
       <Header>
-        <Nav
+        {/* <Nav
           pages={pages}
           setCurrentPage={setCurrentPage}
           currentPage={currentPage}
-        ></Nav>
+        ></Nav> */}
       </Header>
       <main>
         <PageContent >{renderPage()}</PageContent>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,62 +1,82 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import axios from "axios";
+import moment from "moment";
 
-function Home() {
+class Home extends Component {
 
-  // state = {
-  //   Green: 0,
-  //   Red: 0,
-  //   Blue: 0,
-  //   Yellow: 0
-  // }
+  state = {
+    Green: 0,
+    Red: 0,
+    Blue: 0,
+    Yellow: 0,
+    Time: 0
+  }
 
-  const [Green, setGreen] = useState(0);
-  const [Red, setRed] = useState(0);
-  const [Blue, setBlue] = useState(0);
-  const [Yellow, setYellow] = useState(0)
+  // const [Green, setGreen] = useState(0);
+  // const [Red, setRed] = useState(0);
+  // const [Blue, setBlue] = useState(0);
+  // const [Yellow, setYellow] = useState(0)
+  // const [time, setTime] = useState("");
 
-  // const wakeServer = () => {
-  //   axios.get("https://clanwarserver.herokuapp.com/api/wake") 
-  //   .then(response => console.log(response.data))
-  //   .catch(err => console.log(err));
-  // };
+  updateInterval;
+
+  wakeServer = () => {
+    axios.get("https://clanwarserver.herokuapp.com/api/wake") 
+    .then(response => console.log(response.data))
+    .catch(err => console.log(err));
+  };
+
+  getTime = () => {
+    // this.setTime(Date.now().toString());
+    this.setState({Time: moment().format("dddd, MMMM Do YYYY, h:mm a")});
+    console.log(`The time is ${this.state.Time}`);
+  };
 
   
-  const getScores = () => {
+  getScores = () => {
     axios.get("https://clanwarserver.herokuapp.com/api")
     .then((response) => {
-      console.log(response.data)
-      // this.setState({
-      //   Green: response.data[0].points,
-      //   Red: response.data[1].points,
-      //   Blue: response.data[2].points,
-      //   Yellow: response.data[3].points
-      // })
-      setGreen(response.data[0].points);
-      setRed(response.data[1].points);
-      setBlue(response.data[2].points);
-      setYellow(response.data[3].points);
+      // console.log(response.data)
+      this.setState({
+        Green: response.data[0].points,
+        Red: response.data[1].points,
+        Blue: response.data[2].points,
+        Yellow: response.data[3].points
+      })
+      // setGreen(response.data[0].points);
+      // setRed(response.data[1].points);
+      // setBlue(response.data[2].points);
+      // setYellow(response.data[3].points);
+
     })
     .catch((err) => {
       console.log(err)
     })
   };
-
-getScores()
-
-  return (
-    <section className="my-5 text-center">
-      <div className="my-2">
-        <h1>Here are the scores:</h1>
-        <ul>
-          <li className='bg-success'>Green has: {Green}</li>
-          <li className='bg-danger'>Red has: {Red}</li>
-          <li className='bg-primary'>Blue has: {Blue}</li>
-          <li className='bg-warning'>Yellow has: {Yellow}</li>
-        </ul>
-      </div>
-    </section>
-  );
+  
+  componentDidMount() {
+    this.getTime();
+    this.updateInterval = setInterval(() => {
+      this.wakeServer();
+      this.getScores();
+      this.getTime();
+    }, 60000);
   }
-
+  
+  render () {
+    return (
+      <section className="my-5 text-center">
+        <div className="my-2">
+          <h1>Here are the scores as of {this.state.Time}:</h1>
+          <ul>
+            <li className='bg-success text-bold'>Green has: {this.state.Green}</li>
+            <li className='bg-danger'>Red has: {this.state.Red}</li>
+            <li className='bg-primary'>Blue has: {this.state.Blue}</li>
+            <li className='bg-warning'>Yellow has: {this.state.Yellow}</li>
+          </ul>
+        </div>
+      </section>
+    )
+    }
+}
 export default Home;
